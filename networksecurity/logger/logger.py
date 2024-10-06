cdac4bec7c4a740b592003ebcc80777d6c5a5f5c
Sehaj Singh <singhsp7977@gmail.com>
2024-10-05 17:24:24 -0700

    allowed logs to also stream to the terminal

--- networksecurity/logger/logger.py
+++ networksecurity/logger/logger.py
@@ -11,7 +11,10 @@ os.makedirs(logs_path, exist_ok=True)
 LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
 
 logging.basicConfig(
-    filename=LOG_FILE_PATH,
+    handlers=[
+        logging.FileHandler(LOG_FILE_PATH),
+        logging.StreamHandler()
+    ],
     level=logging.INFO,
     format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
 )
