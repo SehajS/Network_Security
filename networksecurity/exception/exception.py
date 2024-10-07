6b1bd0737e8b64136e3c0b771d67c3e9ea426681
Sehaj Singh <singhsp7977@gmail.com>
2024-10-06 17:27:58 -0700

    exception code fixed

--- networksecurity/exception/exception.py
+++ networksecurity/exception/exception.py
@@ -1,9 +1,9 @@
 import sys
 
 class NetworkSecurityException(Exception):
-    def __init__(self, error_message, error_details):
+    def __init__(self, error_message, error_details:sys):
         self.error_message = error_message
-        _, _, exc_tb = sys.exc_info()
+        _, _, exc_tb = error_details.exc_info()
 
         self.lineno = exc_tb.tb_lineno
         self.file_name = exc_tb.tb_frame.f_code.co_filename
