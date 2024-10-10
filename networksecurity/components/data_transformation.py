0ff8d23dcab5e4f83bf18b93516481af2d7f1429
Sehaj Singh <singhsp7977@gmail.com>
2024-10-09 18:11:47 -0700

    code details for data_transformation pipeline added

--- networksecurity/components/data_transformation.py
+++ networksecurity/components/data_transformation.py
@@ -3,6 +3,11 @@ import sys
 import numpy as np
 import pandas as pd
 
+from sklearn.impute import KNNImputer
+from sklearn.pipeline import Pipeline
+
+from networksecurity.utils.main_utils.utils import save_numpy_array_data, save_object
+
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logger.logger import logging
 
@@ -27,10 +32,61 @@ class DataTransformation:
         except Exception as e:
             raise NetworkSecurityException(e, sys)
     
+    def get_data_transformer_object(self):
+        logging.info("Entered get_transformer_object method of DataTransformation class")
+
+        try:
+            imputer: KNNImputer = KNNImputer(
+                **training_pipeline.DATA_TRANSFORMATION_IMPUTER_PARAMS
+            )
+            logging.info(f"Initialized KNN Imputer with params: {training_pipeline.DATA_TRANSFORMATION_IMPUTER_PARAMS}")
+            preprocessor: Pipeline = Pipeline(
+                [("imputer", imputer)]
+            )
+            return preprocessor
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
+    
     def initiate_data_transformation(self) -> DataTransformationArtifact:
         logging.info("Initiating Data Transformation Pipeline")
 
         try:
-            pass
+            train_df = DataTransformation.read_data(self.data_validation_artifact.valid_train_file_path)
+            test_df = DataTransformation.read_data(self.data_validation_artifact.valid_test_file_path)
+
+            preprocessor = self.get_data_transformer_object()
+
+            # prepare training dataframes by separating target_column
+            input_feature_train_df = train_df.drop(columns=[training_pipeline.TARGET_COLUMN])
+            target_feature_train_df = train_df[training_pipeline.TARGET_COLUMN]
+            target_feature_train_df = target_feature_train_df.replace(-1, 0)
+
+            # prepare testing dataframes by separating target_column
+            input_feature_test_df = test_df.drop(columns=[training_pipeline.TARGET_COLUMN])
+            target_feature_test_df = test_df[training_pipeline.TARGET_COLUMN]
+            target_feature_test_df = target_feature_test_df.replace(-1, 0)
+
+            # Fit the preprocessor on train data and transform the test data
+            preprocessor_obj = preprocessor.fit(input_feature_train_df)
+            transformed_input_train_feature = preprocessor_obj.transform(input_feature_train_df)
+            transformed_input_test_feature = preprocessor_obj.transform(input_feature_test_df)
+
+            train_arr = np.c_[transformed_input_train_feature, np.array(target_feature_train_df)]
+            test_arr = np.c_[transformed_input_test_feature, np.array(target_feature_test_df)]
+
+            # save train_arr and test_arr
+            save_numpy_array_data(self.data_transformation_config.transformed_train_file_path, array=train_arr)
+            save_numpy_array_data(self.data_transformation_config.transformed_test_file_path, array=test_arr)
+            # save the preprocessor object as well
+            save_object(self.data_transformation_config.transformed_object_file_path, preprocessor_obj)
+
+            data_transformation_artifact = DataTransformationArtifact(
+                transformed_train_file_path=self.data_transformation_config.transformed_train_file_path,
+                transformed_test_file_path=self.data_transformation_config.transformed_test_file_path,
+                transformed_object_file_path=self.data_transformation_config.transformed_object_file_path
+            )
+            logging.info(f"Data Transformation Completed, artifacts: {data_transformation_artifact}")
+            return data_transformation_artifact
+
         except Exception as e:
             raise NetworkSecurityException(e, sys)
--- networksecurity/utils/main_utils/utils.py
+++ networksecurity/utils/main_utils/utils.py
@@ -3,6 +3,7 @@ import sys
 
 import yaml
 import dill
+import pickle
 import numpy as np
 import pandas as pd
 
@@ -27,5 +28,26 @@ def write_yaml_file(file_path: str, content: object, repalce: bool = False):
         os.makedirs(os.path.dirname(file_path), exist_ok=True)
         with open(file_path, "w") as file:
             yaml.dump(content, file)
+    except Exception as e:
+        raise NetworkSecurityException(e, sys)
+    
+
+def save_numpy_array_data(file_path: str, array: np.array):
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+        with open(file_path, 'wb') as file_obj:
+            np.save(file_obj, array)
+    except Exception as e:
+        raise NetworkSecurityException(e, sys)
+    
+
+def save_object(file_path: str, obj: object) -> None:
+    try:
+        logging.info(f"Saving Object at {file_path}")
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        with open(file_path, 'wb') as file_obj:
+            pickle.dump(obj, file_obj)
+        logging.info(f"Object saved at {file_path}")
     except Exception as e:
         raise NetworkSecurityException(e, sys)
