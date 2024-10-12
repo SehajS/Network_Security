783140d07534fff24f3f9a4825e72d7b8ecce4f2
Sehaj Singh <singhsp7977@gmail.com>
2024-10-12 13:18:07 -0700

    model evaluation component structure added

--- networksecurity/components/model_evaluation.py
+++ networksecurity/components/model_evaluation.py
@@ -1,3 +1,27 @@
+import os
+import sys
+import pandas as pd
+
+from networksecurity.exception.exception import NetworkSecurityException
+from networksecurity.logger.logger import logging
+
+from networksecurity.entity.artifact_entity import DataTransformationArtifact, ModelTrainerArtifact, ModelEvaluationArtifact
+from networksecurity.entity.config_entity import ModelEvaluationConfig
+from networksecurity.utils.ml_utils.metrics.classification_metrics import get_classification_score
+from networksecurity.utils.ml_utils.model.estimator import NetworkModel, ModelResovler
+from networksecurity.utils.main_utils.utils import load_object, save_object, read_yaml_file
+from networksecurity.constant import training_pipeline
+
+
 class ModelEvaluation:
     def __init__(self):
-        pass
+        try:
+            pass
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
+        
+    def initiate_model_evalution(self)-> ModelEvaluationArtifact:
+        try:
+            pass
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
