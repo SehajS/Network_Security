beea370173e8ed097ca7d4d5fede822d2d8ad6a2
Sehaj Singh <singhsp7977@gmail.com>
2024-10-13 22:56:35 -0700

    model pusher componenet structure added

--- networksecurity/components/model_pusher.py
+++ networksecurity/components/model_pusher.py
@@ -1,3 +1,26 @@
+import os
+import sys
+import shutil
+
+from networksecurity.exception.exception import NetworkSecurityException
+from networksecurity.logger.logger import logging
+
+from networksecurity.entity.artifact_entity import ModelEvaluationArtifact, ModelPusherArtifact
+from networksecurity.entity.config_entity import ModelPusherConfig
+
+from networksecurity.utils.ml_utils.metrics.classification_metrics import get_classification_score
+from networksecurity.utils.main_utils.utils import save_object, load_object, write_yaml_file
+
+
 class ModelPusher:
     def __init__(self):
-        pass
+        try:
+            pass
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
+        
+    def initiate_model_pusher(self):
+        try:
+            pass
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
