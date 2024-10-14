4ff5c62353b11d57e4f2f71684acbb220d5ea734
Sehaj Singh <singhsp7977@gmail.com>
2024-10-14 11:24:17 -0700

    model pusher component finished

--- networksecurity/components/model_pusher.py
+++ networksecurity/components/model_pusher.py
@@ -13,14 +13,30 @@ from networksecurity.utils.main_utils.utils import save_object, load_object, wri
 
 
 class ModelPusher:
-    def __init__(self):
+    def __init__(self, model_pusher_config: ModelPusherConfig, model_eval_artifact: ModelEvaluationArtifact):
         try:
-            pass
+            self.model_pusher_config = model_pusher_config
+            self.model_eval_artifact = model_eval_artifact
         except Exception as e:
             raise NetworkSecurityException(e, sys)
         
     def initiate_model_pusher(self):
         try:
-            pass
+            logging.info("Intiating Model Pusher Component")
+            trained_model_path = self.model_eval_artifact.trained_model_path
+
+            # Creating model pusher dir to save model
+            model_file_path = self.model_pusher_config.model_file_path
+            os.makedirs(os.path.dirname(model_file_path), exist_ok=True)
+            shutil.copy(src=trained_model_path, dst=model_file_path)
+
+            # Saving model dir
+            saved_model_path = self.model_pusher_config.saved_model_path
+            os.makedirs(os.path.dirname(saved_model_path), exist_ok=True)
+            shutil.copy(src=trained_model_path, dst=saved_model_path)
+
+            model_pusher_artifact = ModelPusherArtifact(saved_model_path=saved_model_path, model_file_path=model_file_path)
+            logging.info(f"Model pusher finished, artifact: {model_pusher_artifact}")
+            return model_pusher_artifact
         except Exception as e:
             raise NetworkSecurityException(e, sys)
